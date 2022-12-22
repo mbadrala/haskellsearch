914db632c4b9142a8b400c3ce5d8bd9e810ff223
ᠮᠥᠩᠬᠡᠪᠠᠳᠠᠷᠠᠯ <15154972+mbadrala@users.noreply.github.com>
2022-12-22 22:13:14 +0800

    Read from example texts

---
+++ run_example.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Searching for 'king' in example text files"
+cabal run
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import System.IO ( openFile, IOMode(ReadMode), hGetContents )
+import System.Directory ( getDirectoryContents )
+import Control.Monad ( forM_ )
+import Data.List ( isInfixOf )
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+    let directoryPath = "example"
+    files <- getDirectoryContents directoryPath 
+    forM_ files $ \s -> do
+        if isInfixOf ".txt" s 
+            then do
+                let filepath = directoryPath ++ "/" ++ s
+                handle <- openFile filepath ReadMode
+                contents <- hGetContents handle
+                print contents
+        else
+            return ()
